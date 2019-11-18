ed2a042bf5e69e76f29ff531b16d1df5758c287c
Lucas Eduardo <lucased78@hotmail.com>
2019-11-18 15:47:53 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/category_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: CategoryScreen(),
+    );
+  }
+}
